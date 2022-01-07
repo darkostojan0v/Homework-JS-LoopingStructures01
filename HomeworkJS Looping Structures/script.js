75af94e9360adb27f265d316ea82fe794eafffba
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-01-07 17:01:38 +0100

    Add files via upload

---
+++ HomeworkJS Looping Structures/script.js
@@ -0,0 +1,11 @@
+console.log('Looping Structures');
+
+for(var x = 0; x <= 20; x++) {
+
+    if(x%2 == 0) {
+        console.log(x + " is even\n ");
+    }
+    else {
+        console.log(x + " is odd");
+    }
+}
